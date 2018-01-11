8e8e22644a335dd35427e9dd153915bcbfe39651
JDA\j1011436 <raghavendra.badveeti@jda.com>
2018-01-11 18:16:29 +0530

    Removed comment

--- src/com/rgv/creational/design/patterns/SingletonPattern.java
+++ src/com/rgv/creational/design/patterns/SingletonPattern.java
@@ -1,8 +1,7 @@
 package com.rgv.creational.design.patterns;
 
 public class SingletonPattern {
-
-    // Sample Program
+    
     public static void main(String args[]) {
         System.out.println("");
     }
