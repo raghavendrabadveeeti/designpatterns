27c9abbacca7670151504d4178851fdce830b64d
JDA\j1011436 <raghavendra.badveeti@jda.com>
2018-01-11 18:12:47 +0530

    Added Comment

--- src/com/rgv/creational/design/patterns/SingletonPattern.java
+++ src/com/rgv/creational/design/patterns/SingletonPattern.java
@@ -2,6 +2,7 @@ package com.rgv.creational.design.patterns;
 
 public class SingletonPattern {
 
+    // Sample Program
     public static void main(String args[]) {
         System.out.println("");
     }
